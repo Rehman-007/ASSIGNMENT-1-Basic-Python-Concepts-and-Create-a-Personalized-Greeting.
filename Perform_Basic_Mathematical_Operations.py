17484c6a6e2bc95e8ee528838b95ceb4461b5971
Rehman-007 <abdul.rehman314488@gmail.com>
2025-08-17 12:03:48 +0530

    Add files via upload

---
+++ Perform_Basic_Mathematical_Operations.py
@@ -0,0 +1,12 @@
+#Here we will get input from user
+
+num1 = int(input("Enter first number here : "))
+num2 = int(input("Enter second number here : "))
+
+#This is the calculation part 
+
+print()
+print("Addition : ",num1+num2)
+print("Subtraction : ",num1-num2)
+print("Multiplication : ", num1*num2)
+print("Division : ", num1/num2)
