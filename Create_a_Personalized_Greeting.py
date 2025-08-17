86730346fabedb0b9e05b796082333e624757178
Rehman-007 <abdul.rehman314488@gmail.com>
2025-08-17 12:06:15 +0530

    Add files via upload

---
+++ Create_a_Personalized_Greeting.py
@@ -0,0 +1,12 @@
+#Taking input from user
+
+x = input("Enter your first name here : ")
+y = input("Enter your last name here : ")
+
+#Here we are adding both name 
+
+full_name = (x + " " + y)
+
+#Displaying the final result
+
+print(f"Hello, {full_name} welcome to the python program.")
